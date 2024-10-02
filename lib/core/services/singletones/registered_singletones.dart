4967ef7459c1c87f318c0918e05ab400644288c6
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-10-02 08:25:40 +0300

    set up service locator

---
+++ lib/core/services/singletones/registered_singletones.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_project/core/services/remote/api/api_helper.dart';
+import 'package:bookly_project/core/services/remote/api/api_helper_implementation.dart';
+import 'package:bookly_project/features/home/data/repos/home_repo_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+class RegisteredSingleton {
+  static Future<void> registerInstances() async {
+    getIt.registerLazySingleton<ApiHelper>(() => ApiHelperImplementation()); //Registered the API_HELPER Because we need to use it in to the repositories
+
+    getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(apiHelper: getIt<ApiHelper>(),));
+  }
+}
