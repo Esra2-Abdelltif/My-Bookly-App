fd977d43edffeaf0c4bac20f08172b08c7bb82b1
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-10-05 07:59:50 +0300

    Create App Localizations Cubit

---
+++ lib/features/book_app/presentation/manger/books_app_cubit/books_app_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:bookly_project/core/services/singletones/registered_singletones.dart';
+import 'package:bookly_project/core/utils/constants/app_strings.dart';
+import 'package:bookly_project/features/book_app/data/repos/app_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'books_app_state.dart';
+
+class LocaleCubit extends Cubit<LocaleState> {
+  LocaleCubit() : super(const ChangeLocaleState(Locale(AppStrings.englishCode)));
+
+  String currentLangCode = AppStrings.englishCode;
+
+  Future<void> getSavedLang() async {
+    final response = await getIt<BaseLangRepository>().getSavedLang();
+    response.fold((failure) => debugPrint(AppStrings.cacheFailure), (value) {
+      currentLangCode = value;
+      print(value);
+      emit(ChangeLocaleState(Locale(currentLangCode)));
+    });
+  }
+
+  Future<void> _changeLang(String langCode) async {
+    final response =
+        await getIt<BaseLangRepository>().changeLang(langCode: langCode);
+    response.fold((failure) => debugPrint(AppStrings.cacheFailure), (value) {
+      currentLangCode = langCode;
+      emit(ChangeLocaleState(Locale(currentLangCode)));
+    });
+  }
+
+  void toEnglish() => _changeLang(AppStrings.englishCode);
+
+  void toArabic() => _changeLang(AppStrings.arabicCode);
+}
